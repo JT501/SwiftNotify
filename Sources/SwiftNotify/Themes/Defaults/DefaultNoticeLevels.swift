5b35d6eb18badde4f4ee83a0766c06ca8ef8cabc
Kiu Cheong Choi <hallelujahbaby@gmail.com>
2022-03-03 22:33:31 +0800

    feat: New Enum DefaultNoticeLevels

---
+++ Sources/SwiftNotify/Themes/Defaults/DefaultNoticeLevels.swift
@@ -0,0 +1,14 @@
+//
+// Created by Johnny Choi on 3/3/2022.
+// Copyright (c) 2022 Johnny Tsoi@JT501. All rights reserved.
+//
+
+import Foundation
+
+/// SwiftNotify default notice levels
+public enum DefaultNoticeLevels: String {
+    case success
+    case fail
+    case info
+    case warning
+}

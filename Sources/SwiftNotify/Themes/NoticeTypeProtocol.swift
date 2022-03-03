f548d478f5e984c9b539995e917a619c6b90517c
Kiu Cheong Choi <hallelujahbaby@gmail.com>
2022-03-03 19:20:09 +0800

    feat: Add Theme Protocols
    Notice Type Protocol & Theme Protocol

--- Sources/SwiftNotify/Themes/NoticeTypeProtocol.swift
+++ Sources/SwiftNotify/Themes/NoticeTypeProtocol.swift
@@ -7,12 +7,21 @@ import Foundation
 
 public protocol NoticeTypeProtocol {
     var backgroundColor: UIColor { get }
-    var titleTextColor: UIColor { get }
-    var titleBackgroundColor: UIColor { get }
-    var bodyTextColor: UIColor { get }
-    var bodyBackgroundColor: UIColor { get }
+    var titleTextColor: UIColor? { get }
+    var titleBackgroundColor: UIColor? { get }
+    var bodyTextColor: UIColor? { get }
+    var bodyBackgroundColor: UIColor? { get }
     var iconImage: UIImage? { get }
     var iconImageTintColor: UIColor? { get }
+}
+
+/// Optional protocol variables with default values
+extension NoticeTypeProtocol {
+    public var titleTextColor: UIColor? { nil }
+    public var titleBackgroundColor: UIColor? { nil }
+    public var bodyTextColor: UIColor? { nil }
+    public var bodyBackgroundColor: UIColor? { nil }
+    public var iconImageTintColor: UIColor? { nil }
 }
 
 public enum DefaultNoticeType: String {
